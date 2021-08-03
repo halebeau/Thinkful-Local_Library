dc3448185a40e2bdb18573d2fd26bf193cfe741d
Beau Hale <halebeau3@gmail.com>
2021-08-03 11:30:21 -0700

    Finished first 2 functions in home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,6 +1,16 @@
-function getTotalBooksCount(books) {}
+function getTotalBooksCount(books) {
+  const totalBooks = books.map((book) => books); 
+  return totalBooks.length
+}
 
-function getTotalAccountsCount(accounts) {}
+function getTotalAccountsCount(accounts) {
+  
+  const result = accounts.reduce((account) => {
+  account = accounts.map((account) => account);
+  return account.length;
+  }, 0);
+  return result; 
+}
 
 function getBooksBorrowedCount(books) {}
 
