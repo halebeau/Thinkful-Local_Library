ca0dabe82ed24569029755eb232e4827825deebf
Beau Hale <halebeau3@gmail.com>
2021-08-02 19:11:27 -0700

    Books functions completed

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,36 @@
-function findAuthorById(authors, id) {}
+//this function returns an array of authors with the given ID
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id );
+}
 
-function findBookById(books, id) {}
+//this function returns an array of the books with the given ID
+function findBookById(books, id) {
+  return books.find(book => book.id.includes(id));
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+//this function returns an array of two arrays. 
+//1. an array of the available books
+//2. an array of the books currently borrowed
+function partitionBooksByBorrowedStatus(books) {
+  const availableList = books.filter((book) => book.borrows[0].returned === true);
+  const borrowedList = books.filter((book) => book.borrows[0].returned === false);
+  return [borrowedList, availableList];
+}
 
-function getBorrowersForBook(book, accounts) {}
+//this function returns an array of all of the transactions within a books borrows array
+//the result includes the account objects information
+function getBorrowersForBook(book, accounts) {
+  const result = [];
+  for (let account of accounts) {
+    for (let i = 0; i < book.borrows.length; i++) {
+      if(account.id === book.borrows[i].id) {
+        const returned = book.borrows[i].returned
+        result.push({...account, returned })
+      }
+    }
+  }
+  return result.slice(0,10);
+}
 
 module.exports = {
   findAuthorById,
