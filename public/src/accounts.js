8bd0961c8fecb2fc8bc83a77db46ba35797191e0
Beau Hale <halebeau3@gmail.com>
2021-08-02 17:55:12 -0700

    Fixed: got Accounts page to pass tests

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,54 +1,67 @@
+//this function finds the accounts by the given ID
 function findAccountById(accounts, id) {
-  return accounts.find((account) => account.id === id);
+  return accounts.find((account) => account.id.includes(id));
 }
 
+//this function sorts the accounts by last name
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((accountA, accountB) => {
-    const lastNameA = accountA.name.last;
-    const lastNameB = accountB.name.last;
-    return lastNameA.toLowerCase() < lastNameB.toLowerCase() ? -1 : 1;
-  });
+  return accounts.sort((lastA, lastB) =>
+    lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1
+  );
 }
 
-function numberOfBorrows(account, books) {
-  const { id } = account;
-  let total = 0;
 
-  for (let book in books) {
-    const { borrows } = books[book];
-    borrows.forEach((element) => {
-      if (element.id === id) {
-        total++;
-      }
-    });
-  }
-  return total;
+
+function getTotalNumberOfBorrows(account, books) {
+
+  let result = 0;
+  const booksBorrowedByAccount = books.forEach((book) => {
+    //loops through the borrows array to check if the accounts ID matches
+    if (!!book.borrows) {
+      book.borrows.forEach((accounts) => {
+        if (accounts.id === account.id) {
+          result++;
+        }
+      });
+    }
+  });
+  return result;
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  const accountId = account.id;
-  let result = [];
+//Creates empty array to be filled with borrowed books
+  const borrowedBooks = [];
 
-  result = books.filter((book) => {
-    return book.borrows.some((borrow) => borrow.id === accountId && !borrow.returned);
+  books.forEach((book) => {
+    let bookBorrows = book.borrows;
+
+//loops through the borrows array to check if the borrowers ID matches the accounts ID
+//with the condition that the book has not been returned
+    bookBorrows.forEach((borrow) => {
+      if (borrow.id === account.id && !borrow.returned) {
+        borrowedBooks.push(book);
+      }
+    });
   });
 
-  result = result.map((book) => {
-    const author = getAuthorById(authors, book.authorId);
-    const newBook = {
-      ...book,
-      author,
-    };
-
-    return newBook;
+  //returns a new object within an array of all of the borrowed books 
+  let result = borrowedBooks.map((book) => {
+    return { ...book, author: getAuthor(book, authors) };
   });
 
   return result;
 }
 
+// Helper function
+// Returns author object
+function getAuthor(book, authors) {
+  const author = authors.find((author) => author.id === book.authorId);
+  return author;
+}
+
 module.exports = {
   findAccountById,
   sortAccountsByLastName,
-  numberOfBorrows,
+  getTotalNumberOfBorrows,
   getBooksPossessedByAccount,
 };
